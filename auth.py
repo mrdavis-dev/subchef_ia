b34375f85f0901a4b0abc80b4a2192e09eaead50
mrdavis-dev <david1405.df@gmail.com>
2024-06-24 00:23:15 -0500

    fix google-auth

--- auth.py
+++ auth.py
@@ -10,30 +10,39 @@ client_secret = os.getenv("client_secret")
 redirect_uri = os.getenv("redirect_uri")
 
 # Inicializar la autenticación de Google
-login = Google_auth(clientId=client_id, clientSecret=client_secret, redirect_uri=redirect_uri)
+google_auth = Google_auth(clientId=client_id, clientSecret=client_secret, redirect_uri=redirect_uri)
 
+# Conexión a la base de datos
 db = get_db_connection()
 users_collection = db['users']
 
 def authenticate():
-    if login == "authenticated":
-            # Si el usuario está autenticado, obtener el email del estado de la sesión
-            email = st.session_state.get('email')
-            if email:
-                user = users_collection.find_one({'email': email})
-                if user:
-                    st.write(f"Bienvenido, {email}!")
-                else:
-                    # Guardar el nuevo usuario en la base de datos
-                    users_collection.insert_one({'email': email})
-                    st.write(f"¡Bienvenido, nuevo usuario! Su correo electrónico '{email}' ha sido registrado.")
+    if "authenticated" not in st.session_state:
+        st.session_state["authenticated"] = False
+
+    if not st.session_state["authenticated"]:
+        login_status = google_auth.login()
+        if login_status == "authenticated":
+            st.session_state["authenticated"] = True
+            st.session_state["email"] = google_auth.email
+            email = st.session_state["email"]
+            
+            # Verificar si el usuario ya existe en la base de datos
+            user = users_collection.find_one({"email": email})
+            if user:
+                st.write(f"Bienvenido, {email}!")
             else:
-                st.error("Authentication failed")
-                st.stop()
+                # Guardar el nuevo usuario en la base de datos
+                users_collection.insert_one({"email": email})
+                st.write(f"¡Bienvenido, nuevo usuario! Su correo electrónico '{email}' ha sido registrado.")
+        else:
+            st.error("Autenticación fallida. Por favor, intenta nuevamente.")
+            st.stop()
+        
+
     else:
-        st.warning("Necesitas iniciar sesión")
+        email = st.session_state["email"]
+        st.write(f"Bienvenido, {email}!")
 
+# Llamar a la función de autenticación en el archivo principal
 authenticate()
-
-
-
