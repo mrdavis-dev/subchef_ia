f2ebbd7517dd8815247320e3192db3638fb0ed5f
mrdavis-dev <david1405.df@gmail.com>
2024-06-23 23:28:28 -0500

    fixes login and database

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 @st.cache_resource
 def get_db_connection():
-    uri = os.getenv('MONGO_URI')
+    uri = os.getenv('MONGO_URL')
     client = MongoClient(uri)
     db = client.get_database("test")
     return db
