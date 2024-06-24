7ccb3a7c6028051e919cd27fbfcf03e0ebe67ce9
mrdavis-dev <david1405.df@gmail.com>
2024-06-23 22:47:36 -0500

    update auth google

--- auth.py
+++ auth.py
@@ -1,28 +1,39 @@
 # auth.py
+import os
 import streamlit as st
-from google_auth_st import add_auth
+from StreamlitGauth.google_auth import Google_auth
 from db import get_db_connection
 
+# Configurar las credenciales de Google OAuth
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+redirect_uri = os.getenv("redirect_uri")
 
-st.warning("Necesitas iniciar sesion")
+# Inicializar la autenticación de Google
+login = Google_auth(clientId=client_id, clientSecret=client_secret, redirect_uri=redirect_uri)
 
 db = get_db_connection()
 users_collection = db['users']
 
 def authenticate():
-
-    add_auth()
-    if 'email' not in st.session_state:
-        st.error("Authentication failed")
-        st.stop()
+    if login == "authenticated":
+            # Si el usuario está autenticado, obtener el email del estado de la sesión
+            email = st.session_state.get('email')
+            if email:
+                user = users_collection.find_one({'email': email})
+                if user:
+                    st.write(f"Bienvenido, {email}!")
+                else:
+                    # Guardar el nuevo usuario en la base de datos
+                    users_collection.insert_one({'email': email})
+                    st.write(f"¡Bienvenido, nuevo usuario! Su correo electrónico '{email}' ha sido registrado.")
+            else:
+                st.error("Authentication failed")
+                st.stop()
     else:
-        user = users_collection.find_one({'email': st.session_state.email})
-        if user:
-            st.write(f"Bienvenido, {st.session_state.email}!")
-        else:
-            # Guardar el nuevo usuario en la base de datos
-            users_collection.insert_one({'email': st.session_state.email})
-            st.write(f"¡Bienvenido, nuevo usuario! Su correo electrónico '{st.session_state.email}' ha sido registrado.")
+        st.warning("Necesitas iniciar sesión")
+
+authenticate()
 
 
 
